38b81ee69612cc519d6465366189a641315cb2a2
EduDinizO <edu_diniz@hotmail.com>
2017-12-26 15:34:05 -0200

    animation jquery

--- omnifood/source/js/script.js
+++ omnifood/source/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     /*$('h1').click(function () {
         $(this).css('background-color', '#ff0000')
     })*/
-
+    // STICKY NAVIGATION
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -14,10 +14,65 @@ $(document).ready(function () {
         offset: '60px'
     });
 
+    // SCROLL ON BUTTONS
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    })
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    })
+
     /*var waypoints = $('#handler-first').waypoint(function (direction) {
         notify(this.element.id + ' hit 25% from top of window')
     }, {
         offset: '25%'
     })*/
 
+    // NAV BAR SCROLL
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    // ANIMATE FADEIN ROW ON SCROLL
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    })
+
+    // ANIMATE FDE UP IPHONE IMG
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    })
+
+    // ANIMATE FADE IN CITIES
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    })
+
+    // ANIMATE PULSE PRICE PLAN
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    })
 })
