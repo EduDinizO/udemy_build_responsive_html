97349e937cfd1675a2288cc5b0432ea45785c620
EduDinizO <edu_diniz@hotmail.com>
2017-12-26 23:09:42 -0200

    nav bar mobile

--- omnifood/source/js/script.js
+++ omnifood/source/js/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-    /*$('h1').click(function () {
-        $(this).css('background-color', '#ff0000')
-    })*/
     // STICKY NAVIGATION
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
@@ -20,6 +17,7 @@ $(document).ready(function () {
             scrollTop: $('.js--section-plans').offset().top
         }, 1000);
     })
+
     $('.js--scroll-to-start').click(function () {
         $('html, body').animate({
             scrollTop: $('.js--section-features').offset().top
@@ -74,5 +72,19 @@ $(document).ready(function () {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '50%'
+    })
+
+    // MOBILE NAV
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
     })
 })
